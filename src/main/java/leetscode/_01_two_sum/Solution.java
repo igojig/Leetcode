b52f7403e31b5dac7c2bc47ab41cbaa1e0753981
igojig <igojig@mail.ru>
2024-04-20 12:57:04 +0300

    add task 001_Two_Sum I found One-pass HashTable solution

---
+++ src/main/java/leetscode/_01_two_sum/Solution.java
@@ -0,0 +1,25 @@
+package leetscode._01_two_sum;
+
+import java.util.HashMap;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+                int findable=target-nums[i];
+                int foundIndex = map.getOrDefault(findable, -1);
+                if (foundIndex != -1) {
+                    return new int[]{i, foundIndex};
+                } else {
+                    map.put(nums[i], i);
+                }
+            }
+
+        // never
+        return new int[]{-1, -1};
+
+    }
+
+}
--- src/main/java/leetscode/two_sum/Main.java
+++
@@ -1,7 +0,0 @@
-package leetscode.two_sum;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
