ef4ce16e88b95c450ddfea302e63d01bcb286808
igojig <igojig@mail.ru>
2024-04-25 11:36:43 +0300

    14. Longest common prefix

---
+++ src/main/java/leetscode/_14_longest_common_prefix/Main.java
@@ -0,0 +1,21 @@
+package leetscode._14_longest_common_prefix;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        String[] strs = {"flower","flow"};
+
+        String ans=s.longestCommonPrefix(strs);
+
+        System.out.println(ans);
+
+//        String s1="helper";
+//        String s2="hesswert";
+//
+//        String res=s.helper(s1, s2);
+//
+//        System.out.println(res);
+
+
+    }
+}
