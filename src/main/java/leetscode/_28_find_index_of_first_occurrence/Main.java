d208b76f13d2951ed09db108681c2e8794d2f6e7
igojig <igojig@mail.ru>
2024-04-29 00:15:47 +0300

    28. find index of first occurrence

---
+++ src/main/java/leetscode/_28_find_index_of_first_occurrence/Main.java
@@ -0,0 +1,11 @@
+package leetscode._28_find_index_of_first_occurrence;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        String s1="mississippi";
+        String s2="pi";
+        int res=s.strStr(s1, s2);
+        System.out.println(res);
+    }
+}
