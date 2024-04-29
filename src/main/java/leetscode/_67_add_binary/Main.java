704c55d29f6df8246b7b6b43c31fec26edf08e67
igojig <igojig@mail.ru>
2024-04-29 23:22:31 +0300

    67. Add binary

---
+++ src/main/java/leetscode/_67_add_binary/Main.java
@@ -0,0 +1,11 @@
+package leetscode._67_add_binary;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        String s1="1011";
+        String s2="10011";
+        String res=s.addBinary(s1, s2);
+        System.out.println(res);
+    }
+}
