a89940a0c4c18488b8bcb0b58b11878b6a445d14
igojig <igojig@mail.ru>
2024-04-30 17:00:56 +0300

    69. Sqrt(x)

---
+++ src/main/java/leetscode/_69_sqrt_x/Main.java
@@ -0,0 +1,9 @@
+package leetscode._69_sqrt_x;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        int res=s.mySqrt(99);
+        System.out.println(res);
+    }
+}
