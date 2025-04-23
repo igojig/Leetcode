0dd8c681f8e15cca425765bebcfe537d438b3956
Igor Zhigachev <igojig@mail.ru>
2025-04-23 23:24:11 +0300

    15. 3 sum

---
+++ src/main/java/leetscode/medium/_15_3sum/Main.java
@@ -0,0 +1,11 @@
+package leetscode.medium._15_3sum;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+//        int[] arr={-1,0,1,2,-1,-4};
+        int[] arr={0,0,0,0};
+        var l=s.threeSum(arr);
+        System.out.println(l);
+    }
+}
---
+++ src/main/java/leetscode/medium/_15_3sum/Solution.java
@@ -0,0 +1,29 @@
+package leetscode.medium._15_3sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    break;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
