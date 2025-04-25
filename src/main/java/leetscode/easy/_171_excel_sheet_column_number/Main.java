67832e361abf5596ca2ae16ea06141241aad20ea
Igor Zhigachev <igojig@mail.ru>
2025-04-25 19:56:07 +0300

    171. Excel sheet column number

---
+++ src/main/java/leetscode/easy/_171_excel_sheet_column_number/Main.java
@@ -0,0 +1,10 @@
+package leetscode.easy._171_excel_sheet_column_number;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        String str="ZY";
+        int res=s.titleToNumber(str);
+        System.out.println(res);
+    }
+}
